400fcd64fa9e1d2c05b88d462945d02cb6b15c23
vlnote62 <vlnote62>
2023-12-07 17:44:24 -0300

    Finalizando as operações do arquivo js

--- js/index.js
+++ js/index.js
@@ -4,19 +4,30 @@ function IncrementaClique()
     let incremento = document.getElementById("upgradePontuacao");
     let novaPontuacao = parseInt(pontuacao.innerText) + parseInt(incremento.innerText);
     pontuacao.innerText= novaPontuacao;
+    if(parseInt(novaPontuacao) >= 500){
+        alert('Parabéns!, você ganhou!!');
+    }
 }
 
 function DefineUpgrade(){
     let pontuacao = document.getElementById("pontuacao");
     let incremento = document.getElementById("upgradePontuacao");
-    var inputElement = document.getElementById("inputGrade");
+    var inputElement = document.getElementById("inputUpgrade");
+    console.log(inputElement.value);
     let novoIncremento;
-    if(parseInt(inputElement.value) * 2 <= parseInt(pontuacao)){
-        novoIncremento = inputElement.value;
-        pontuacao.innerText = parseInt(pontuacao) - parseInt(inputElement.value) * 2;
+    if(inputElement.value != null && inputElement.value != ''){
+        console.log(inputElement.value)
+        if(parseInt(inputElement.value) * 2 <= parseInt(pontuacao.innerText)){
+            novoIncremento = inputElement.value;
+            pontuacao.innerText = parseInt(pontuacao.innerText) - parseInt(inputElement.value) * 2;
+            incremento.innerText= novoIncremento;
+        }
+        else{
+            alert('Você não possui pontos suficientes!');
+        }
     }
     else{
-        alert('Você não possui pontos suficientes!');
+        alert('Insira um valor válido!');
     }
-    incremento.innerText= novoIncremento;
+    
 }
